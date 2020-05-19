4715a09bd1b472c09c07bbbb2fe87b477ddf5835
caesar003 <caesarmuksid@gmail.com>
2020-05-20 03:43:18 +0800

    working on some more routes, adding image functionality

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
-
+const multer = require('multer');
+const fs = require('fs');
+const url = require('url');
 const db = knex({
   client: 'pg',
   connection : {
@@ -16,9 +18,25 @@ const db = knex({
 });
 
 const app = express();
-
 app.use(bodyParser.json());
 app.use(cors());
+
+const storage = multer.diskStorage({
+  destination : (req, file, cb) => {
+    cb(null, 'public')
+  },
+  filename : (req, file, cb) => {
+    cb(null, file.originalname);
+  }
+  // filename : (req, file, cb) => {
+  //   cb(null, Date.now() + '-' + file.originalname);
+  // }
+  //
+});
+
+const upload = multer({
+  storage:storage
+}).single('file');
 
 app.get('/', (req, res) => {
   res.send('this is working');
@@ -73,21 +91,80 @@ app.post('/register', (req, res) => {
 });
 
 app.post('/post', (req, res) => {
-  const {post, user_id} = req.body;
+  const {post, user_id, attachment} = req.body;
   return db('posts').insert({
     post: post,
     post_date: new Date(),
     user_id: user_id,
+    atch: attachment,
   }).then(response => {
     res.json(response);
   });
 });
 
 app.get('/post', (req, res)=>{
-  return db.select('*').from('posts').then(users=>{
+  return db.select('*')
+    .from('posts')
+    .orderBy('post_date', 'desc')
+    .then(users=>{
+      res.json(users);
+  })
+});
+
+app.get('/users', (req, res) => {
+  return db.select('*').from('users').then(users=>{
     res.json(users);
   })
 });
+
+app.post('/postMedia', (req, res) => {
+  // console.log(req);
+  // return res.status(200).json('Ok');
+  upload(req, res, (err)=>{
+    if(err instanceof multer.MulterError){
+      return res.status(500).json(err);
+    } else if(err){
+      return res.status(500).json(err);
+    }
+    // console.log(req.file.filename);
+    return res.status(200).send(req.file.filename);
+  });
+});
+
+app.get('/picture/:img', (req, res) => {
+  fs.readFile('./public/'+req.params.img, (err, content) => {
+    if(err){
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      console.log(err);
+      res.end('Image not found');
+    } else {
+      res.writeHead(200, {'Content-Type': 'image/jpg'});
+      res.end(content);
+    }
+  })
+});
+
+// app.put('/userprofile', (req, res) => {
+//   // const {} = req.body;
+//   db('users')
+//   .where('', '=', '')
+//   .update({
+//     key:'value',
+//     key2: 'value'
+//   })
+//   .returning('users')
+//   .then(users=>{
+//     res.json(users);
+//   });
+// })
+
+// app.get('/video/:clip', (req, res) => {
+//
+// })
+
+// app.get('/user', (req, res) => {
+//   db.select('*').from('users').where('')
+// })
 
 /*
   --> to dos endpoints
@@ -95,7 +172,9 @@ app.get('/post', (req, res)=>{
       GET /comments
       POST /comment
       PUT /userprofile
-
+      POST /profile_picture
+      GET /picture
+      POST /profile-update
 */
 
 app.get('/profile/:id', (req, res) => {
@@ -111,14 +190,6 @@ app.get('/profile/:id', (req, res) => {
   })
     .catch(err => res.status(404).json('not found'));
 });
-
-// bcrypt.compare('bacon', hash, function(err, res){
-//   //
-// })
-//
-// bcrypt.compare('veggies', hash, function(err, res){
-//   //
-// })
 
 app.listen(3027, ()=> {
   console.log('app is running on port 3027');
